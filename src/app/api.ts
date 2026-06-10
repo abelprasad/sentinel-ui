708a72d06d06672a96e096d65b26d04181280c7f
abelprasad <abelprasad4@gmail.com>
2026-06-10 00:54:44 -0400

    Trigger deploy with production build

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 export interface AnomalyScore {
   id: number;
@@ -33,7 +34,7 @@ export interface PositionDTO {
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
-  private base = 'http://localhost:8080';
+  private base = environment.apiUrl;
   private token = '';
 
   constructor(private http: HttpClient) {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://100.98.50.85:8888'
+};
